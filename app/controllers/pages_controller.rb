7e763f6bcc184af78adbb8ccac88370c9f99d7dd
dhirajkarn <dhiraj.karn81@gmail.com>
2013-06-13 23:38:52 +0530

    removed default rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,9 @@ class PagesController < ApplicationController
   def contact
   	@title = "Contact"
   end
+
+  def help
+  	@title = "Help"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ BlogApp::Application.routes.draw do
 
   get "pages/contact"
 
+  get "pages/help"
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
